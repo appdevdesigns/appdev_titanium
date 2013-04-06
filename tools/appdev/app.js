d710b3113cce42ed2f109766a0d6e1af45aea5db
Caleb Cox <calebcox314@gmail.com>
2013-04-06 14:10:47 -0500

    Add "install" command to install subcommand dependencies

--- tools/appdev/app.js
+++ tools/appdev/app.js
@@ -1,8 +1,29 @@
 #! /usr/bin/env node
 
+var async = require('async');
 var argv = process.argv.slice(2);
 var coa = require('coa');
-var cmd = coa.Cmd().name(process.argv[1]).title('AppDev Titanium command line tool').helpful();
+var cmd = coa.Cmd().name(process.argv[1]).title('AppDev Titanium command line tool').helpful()
+    .cmd() // inplace subcommand definition
+        .name('install').title('Install dependencies').helpful()
+        .act(function() {
+            var npm = require('npm');
+            npm.load({}, function(err) {
+                if (err) throw err;
+                
+                async.each(commands, function(command, callback) {
+                    npm.localPrefix = command.directory;
+                    npm.commands.update([], callback);
+                }, function(err) {
+                	if (err) throw err;
+                });
+            });
+            // Echo npm output to the console
+            npm.on('log', function(message) {
+                console.log(message);
+            });
+        })
+        .end(); // end subcommand definition
 
 var path = require('path');
 var commands = null;
@@ -13,9 +34,11 @@ fs.readdir(commandsDir, function(err, files) {
     if (err) throw err;
     
     commands = files.map(function(command) {
+        var commandDir = path.join(commandsDir, command);
         return {
             name: command,
-            path: path.join(commandsDir, command, 'command.js')
+            directory: commandDir,
+            path: path.join(commandDir, 'command.js')
         };
     });
     commands.forEach(function(command) {
