6a63435b7a6f56716da6bff5420d2a7ad3071a16
Caleb Cox <calebcox314@gmail.com>
2013-04-06 11:07:52 -0500

    New appdev command line tool that will eventually combine the functionality of the project and genimages tools

---
+++ tools/appdev/app.js
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+
+var argv = process.argv.slice(2);
+var coa = require('coa');
+var cmd = coa.Cmd().name(process.argv[1]).title('AppDev Titanium command line tool').helpful();
+
+var path = require('path');
+var commands = null;
+var commandsDir = path.join(__dirname, 'commands');
+
+var fs = require('fs');
+fs.readdir(commandsDir, function(err, files) {
+    if (err) throw err;
+    
+    commands = files.map(function(command) {
+        return {
+            name: command,
+            path: path.join(commandsDir, command, 'command.js')
+        };
+    });
+    commands.forEach(function(command) {
+        cmd.cmd().name(command.name).end();
+    });
+    cmd.end().run(argv.length ? argv : ['-h']);
+});
