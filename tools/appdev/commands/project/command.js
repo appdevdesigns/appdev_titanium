69236659e02b14155c2c6a9bf4e78b3958f8ab2b
Caleb Cox <calebcox314@gmail.com>
2013-04-25 18:06:22 -0500

    Facilitate generic pre-processing of all loaded commands, in addition to post-processing

--- tools/appdev/commands/project/command.js
+++ tools/appdev/commands/project/command.js
@@ -8,11 +8,11 @@ module.exports.COA = function() {
     this.title('Manage AppDev Titanium projects').helpful().loadCommands({
         rootDirectory: __dirname,
         setupStack: project.setupStack,
-        onCommand: function(cmd) {
+        coaPreProcess: function() {
             // This callback is called for each subcommand that was loaded
             
             // Add a project argument to all subcommands
-            return cmd.arg()
+            return this.arg()
                 .name('project').title('Project')
                 .req() // argument is required
                 .end(); // end argument definition
