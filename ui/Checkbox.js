4418382392ed67ed48ae26f9811a856b7d979aa6
Caleb Cox <calebcox314@gmail.com>
2013-06-25 17:17:34 -0500

    Add support for an optional text overlay over the checkbox image

--- ui/Checkbox.js
+++ ui/Checkbox.js
@@ -11,35 +11,54 @@ module.exports = $.View('AppDev.UI.Checkbox', {
             width: defaultSize,
             height: defaultSize
         },
+        overlayText: null,
         value: false
     }
 }, {
     init: function(options) {
         this.value = this.options.value;
         
-        // Create the checkbox image
-        var checkbox = Ti.UI.createImageView(this.options.createParams);
+        var view = Ti.UI.createView(this.options.createParams);
         
         // The image is the checkbox's view
-        this._super({view: checkbox});
+        this._super({ view: view });
         
         this.addEventListener('click', function(event) {
             // Toggle the checkbox state
             this.setValue(!this.value);
             // Emmulate a change event
-            checkbox.fireEvent('change', {
+            view.fireEvent('change', {
                 source: event.source,
                 type: 'change',
                 value: this.value
             });
         });
-        
+    },
+
+    // Create the child views
+    create: function() {
+        // Create the checkbox image
+        this.add('checkbox', Ti.UI.createImageView({
+            width: Ti.UI.FILL,
+            height: Ti.UI.FILL
+        }));
+        // Create the overlay text label
+        this.add('overlay', Ti.UI.createLabel({
+            width: Ti.UI.FILL,
+            height: Ti.UI.FILL,
+            textAlign: 'center',
+            font: AD.UI.Fonts.small,
+            text: this.options.overlayText
+        }));
+    },
+
+    // Initialize the child views
+    initialize: function() {
         this.update();
     },
     
     update: function() {
-        var checkbox = this.view;
-        checkbox.image = this.value ? '/images/checkbox-checked.png' : '/images/checkbox-unchecked.png';
+        this.getChild('checkbox').image = this.value ? '/images/checkbox-checked.png' : '/images/checkbox-unchecked.png';
     },
     
     setValue: function(value) {
