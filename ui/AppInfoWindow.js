f0dc68554a299a1ad334861a3633f4e10b501e3e
Caleb Cox <calebcox314@gmail.com>
2013-11-25 18:54:10 -0600

    Display the software license text in English, regardless of the user's language

--- ui/AppInfoWindow.js
+++ ui/AppInfoWindow.js
@@ -95,7 +95,17 @@ module.exports = $.Window('AppDev.UI.AppInfoWindow', {
         title: 'preferences',
         callback: 'preferences',
         platform: 'Android'
-    }]
+    }],
+    aboutLicenseText:
+        'Copyright (c) 2013, AppDev Designs\n'+
+        'All rights reserved.\n'+
+        '\n'+
+        'Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:\n'+
+        '    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.\n'+
+        '    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.\n'+
+        '    * Neither the name {0} nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.\n'+
+        '\n'+
+        'THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPDEV DESIGNS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.'
 }, {
     init: function(options) {
         // Initialize the base $.Window object
@@ -152,7 +162,7 @@ module.exports = $.Window('AppDev.UI.AppInfoWindow', {
             width: AD.UI.useableScreenWidth,
             height: Ti.UI.SIZE,
             font: AD.Platform.isAndroid ? AD.UI.Fonts.small : AD.UI.Fonts.mediumSmall,
-            text: $.formatString('aboutLicense', AD.Defaults.application).replace(/\n\n/g, '\n').trim()
+            text: $.formatString(this.constructor.aboutLicenseText, AD.Defaults.application)
         }));
     }
 });
